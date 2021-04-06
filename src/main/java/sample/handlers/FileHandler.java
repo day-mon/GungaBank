781e20479ec8067266fd609408512bb2375ebe7a
Damon <lolmemedon@gmail.com>
2021-04-06 12:55:59 -0400

    Adding FileHandler.java

---
+++ src/main/java/sample/handlers/FileHandler.java
@@ -0,0 +1,102 @@
+package sample.handlers;
+
+import sample.GungaBank;
+import sample.Main;
+import sample.core.objects.User;
+import sample.util.structures.ArrayList;
+import sample.util.structures.HashDictionary;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Objects;
+
+public class FileHandler
+{
+    private static GungaBank gungaBank;
+
+    public FileHandler(GungaBank gungaBank)
+    {
+        this.gungaBank = gungaBank;
+        loadInformation();
+    }
+
+    private static ArrayList<File> getAllFilesWithExt(File dir, String ext)
+    {
+        ArrayList<File> filesWithExt = new ArrayList<>();
+        if (dir.isDirectory())
+        {
+            if (Objects.requireNonNull(dir.listFiles()).length == 0)
+                gungaBank.getLogger().warn("There are no files serializable files");
+            for (File file : dir.listFiles())
+            {
+                String[] fileParts = file.getName().split("\\.");
+                int lastIndex = fileParts.length - 1;
+                if (fileParts[lastIndex].equals(ext))
+                {
+                    filesWithExt.add(file);
+                }
+            }
+
+
+        }
+        else
+        {
+            gungaBank.getLogger().warn(dir.getName() + " is not a directory!");
+            return null;
+        }
+        return filesWithExt;
+    }
+
+    public static void loadInformation()
+    {
+        try
+        {
+            File f = new File("src/main/java/sample/files");
+            if (!f.exists())
+            {
+                f.mkdir();
+                return;
+            }
+            ArrayList<File> files = getAllFilesWithExt(new File("src/main/java/sample/files/"), "ser");
+            int serFiles = 0;
+            try
+            {
+                serFiles = files.size();
+            }
+            catch (NullPointerException npex)
+            {
+                System.out.println("No files exist and serializer input failed.");
+            }
+
+            for (int i = 0; i < serFiles; i++)
+            {
+                FileInputStream fis = new FileInputStream(files.get(i).getAbsolutePath());
+                ObjectInputStream ois = new ObjectInputStream(fis);
+
+                String fileName = files.get(i).getName().split("\\.")[0];
+
+                switch (fileName)
+                {
+                    case "users":
+                        Main.users = (HashDictionary<String, User>) ois.readObject();
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + fileName);
+                }
+
+
+            }
+        }
+        catch (IOException e)
+        {
+            gungaBank.getLogger().error("IOException: " + e.getMessage(), e);
+        }
+        catch(ClassNotFoundException e)
+        {
+            gungaBank.getLogger().error("ClassNotFoundException: " + e.getMessage() ,e);
+        }
+
+    }
+}
